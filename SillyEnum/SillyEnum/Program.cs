5b488b3773ac2d8bb11f52553d67eba28246b027
shellph1sh <LoganGoins@protonmail.com>
2024-08-21 19:16:43 -0500

    Added sillyenum project and removed dead sillyroast code

---
+++ SillyEnum/SillyEnum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SillyEnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("")
+            }
+        }
+    }
+}
--- SillyRoast/SillyRoast/Program.cs
+++ SillyRoast/SillyRoast/Program.cs
@@ -11,14 +11,6 @@ namespace SillyRoast
 {
     internal class Program
     {
-
-        static string GetCurrentDomainPath()
-        {
-            DirectoryEntry de = new DirectoryEntry("LDAP://RootDSE");
-
-            return "LDAP://" + de.Properties["defaultNamingContext"][0].ToString();
-        }
-
         static string[] Query(string query)
         {
             SearchResultCollection results;
