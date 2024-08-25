f903640a56972c417170f9c083da02abbb00fcee
shellph1sh <LoganGoins@protonmail.com>
2024-08-24 21:46:48 -0500

    Added users enum function

--- SillyEnum/SillyEnum/Program.cs
+++ SillyEnum/SillyEnum/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Linq;
 using System.DirectoryServices.ActiveDirectory;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Security.Principal;
+using System.Windows.Markup;
 
 namespace SillyEnum
 {
@@ -18,33 +22,61 @@ namespace SillyEnum
             foreach (DomainController controller in dcs)
             {
                 Console.WriteLine("\n" + controller.Name + "\n===================");
-                Console.WriteLine("Forest: "+ controller.Forest);
+                Console.WriteLine("Forest: " + controller.Forest);
                 Console.WriteLine("IP: " + controller.IPAddress);
                 Console.WriteLine("Version: " + controller.OSVersion + "\n");
-            }            
+            }
         }
 
         static void users()
         {
-            
+            SearchResultCollection results;
+            DirectorySearcher ds = null;
+
+            DirectoryEntry de = new DirectoryEntry();
+            ds = new DirectorySearcher(de);
+            ds.Filter = "(&(ObjectCategory=person)(ObjectClass=user))";
+
+            results = ds.FindAll();
+
+            if (results.Count == 0)
+            {
+                Console.WriteLine("No users found");
+                System.Environment.Exit(0);
+            }
+            foreach (SearchResult sr in results)
+            {
+                Console.WriteLine("\nsamAccountName: " + sr.Properties["samaccountname"][0].ToString());
+                SecurityIdentifier sid = new SecurityIdentifier(sr.Properties["objectSid"][0] as byte[], 0);
+                Console.WriteLine("objectSid: " + sid.Value);
+                Console.WriteLine("distinguishedName: " + sr.Properties["distinguishedname"][0].ToString() + "\n");
+
+                                                           
+            }
         }
 
         static void Main(string[] args)
         {
             if (args.Length > 0)
             {
-                if(args.Contains("--help") || args.Contains("-h"))
+                if (args.Contains("--help") || args.Contains("-h"))
                 {
                     Help();
                 }
                 else if (args.Contains("dclist"))
                 {
-                    dclist(); 
+                    Console.WriteLine("[+] Enumerating Domain Controllers:");
+                    dclist();
+                }
+                else if (args.Contains("users"))
+                {
+                    Console.WriteLine("[+] Enumerating user accounts:");
+                    users();
                 }
             }
             else
             {
-                Console.WriteLine("Please supply a command - Use \"--help\" for a list of commands");
+                Help();
             }
         }
     }
