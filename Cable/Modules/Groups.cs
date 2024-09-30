dc0ae8862d8c84a5025c5d51dcfe6fa9c72e3f2f
shellph1sh <LoganGoins@protonmail.com>
2024-09-30 12:26:17 -0500

    Added group module methods for adding and removing users

--- Cable/Modules/Groups.cs
+++ Cable/Modules/Groups.cs
@@ -9,17 +9,22 @@ namespace Cable.Modules
         public static void GetGroupMembers(string group)
         {
             PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
-
             GroupPrincipal groupPrincipal = GroupPrincipal.FindByIdentity(ctx, group);
             UserPrincipal userPrincipal = UserPrincipal.FindByIdentity(ctx, IdentityType.SamAccountName, group);
 
             if (groupPrincipal == null)
             {
-                Console.WriteLine("[!] Cannot find group");
+                Console.WriteLine("[!] Cannot find group: " + group);
                 return;
             }
 
             PrincipalCollection gcollection = groupPrincipal.Members;
+
+            if(gcollection.Count == 0)
+            {
+                Console.WriteLine("[!] No members found");
+                return;
+            }
 
             Console.WriteLine("[+] Members in group: " + group + "\n");
 
@@ -32,5 +37,45 @@ namespace Cable.Modules
 
             }
         }
+
+        public static void AddToGroup(string user, string group)
+        {
+            PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
+            GroupPrincipal groupPrincipal = GroupPrincipal.FindByIdentity(ctx, group);
+
+            if (groupPrincipal == null)
+            {
+                Console.WriteLine("[!] Cannot find group: " + group);
+                return;
+            }
+
+            Console.WriteLine("[+] Adding user " + user + " to group " + group);
+
+            groupPrincipal.Members.Add(ctx, IdentityType.SamAccountName, user);
+            groupPrincipal.Save();
+
+            Console.WriteLine("[+] Successfully added");
+
+        }
+
+        public static void RemoveFromGroup(string user, string group)
+        {
+            PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
+            GroupPrincipal groupPrincipal = GroupPrincipal.FindByIdentity(ctx, group);
+
+            if (groupPrincipal == null)
+            {
+                Console.WriteLine("[!] Cannot find group: " + group);
+                return;
+            }
+
+            Console.WriteLine("[+] Removing user " + user + " from group " + group);
+
+            groupPrincipal.Members.Remove(ctx, IdentityType.SamAccountName, user);
+            groupPrincipal.Save();
+
+            Console.WriteLine("[+] Successfully removed");
+        }
+
     }
 }
