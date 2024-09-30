0001b7512a8d8011f037ce26a06d11a5842d27da
shellph1sh <LoganGoins@protonmail.com>
2024-09-30 13:17:58 -0500

    Added users module with setSPN method

---
+++ Cable/Modules/Users.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cable.Modules
+{
+    public class Users
+    {
+        public static void setSPN(string spn, string user)
+        {
+            try
+            {
+                SearchResultCollection results;
+
+                DirectoryEntry de = new DirectoryEntry();
+                DirectorySearcher ds = new DirectorySearcher(de);
+
+                string query = "(samaccountname=" + user + ")";
+                ds.Filter = query;
+                results = ds.FindAll();
+
+                if (results.Count == 0)
+                {
+                    Console.WriteLine("[!] Cannot find account");
+                    return;
+                }
+
+                foreach (SearchResult sr in results)
+                {
+                    DirectoryEntry mde = sr.GetDirectoryEntry();
+                    mde.Properties["serviceprincipalname"].Add(spn);
+                    mde.CommitChanges();
+                    Console.WriteLine("[+] SPN added to " + user);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[!] Failed to write servicePrincipalName attrbute: " + ex.Message);
+            }
+        }
+    }
+}
