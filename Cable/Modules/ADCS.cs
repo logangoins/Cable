a830466ef7bf96080622fba607230ff48cba6438
shellph1sh <LoganGoins@protonmail.com>
2024-10-31 12:45:50 -0500

    Changed CA and template formatting

--- Cable/Modules/ADCS.cs
+++ Cable/Modules/ADCS.cs
@@ -34,11 +34,11 @@ namespace Cable.Modules
             {
                 if (sr.Properties.Contains("name"))
                 {
-                    Console.WriteLine("CA: " + sr.Properties["name"][0].ToString());   
+                    Console.WriteLine("[+] Found CA: " + sr.Properties["name"][0].ToString());   
                 }
                 if (sr.Properties.Contains("dnshostname"))
                 {
-                    Console.WriteLine("Hostname: " + sr.Properties["dnshostname"][0].ToString());
+                    Console.WriteLine("\t|__ Hostname: " + sr.Properties["dnshostname"][0].ToString());
                     
                 }
             }
@@ -65,17 +65,16 @@ namespace Cable.Modules
             {
                 if (sr.Properties.Contains("name"))
                 {
-                    Console.WriteLine("Template: " + sr.Properties["name"][0].ToString());
-                    Console.WriteLine("=======================================");
+                    Console.WriteLine("[+] Found Template: " + sr.Properties["name"][0].ToString());
                 }
                 if (sr.Properties.Contains("mspki-enrollment-flag"))
                 {
-                    Console.WriteLine((hCable.msPKIEnrollmentFlag)Convert.ToInt32(sr.Properties["mspki-enrollment-flag"][0].ToString()));
+                    Console.WriteLine("\t|__ mspki-enrollment-flag: " + (hCable.msPKIEnrollmentFlag)Convert.ToInt32(sr.Properties["mspki-enrollment-flag"][0].ToString()));
                 }
 
                 if (sr.Properties.Contains("mspki-certificate-name-flag"))
                 {
-                    Console.WriteLine((hCable.msPKICertificateNameFlag)Convert.ToInt32(sr.Properties["mspki-certificate-name-flag"][0].ToString()));
+                    Console.WriteLine("\t|__ mspki-certificate-name-flag: " + (hCable.msPKICertificateNameFlag)Convert.ToInt32(sr.Properties["mspki-certificate-name-flag"][0].ToString()));
                 }
 
                 if (sr.Properties.Contains("pKIExtendedKeyUsage"))
@@ -83,9 +82,10 @@ namespace Cable.Modules
                     var EKUs = sr.Properties["pKIExtendedKeyUsage"];
                     if (EKUs.Count > 0)
                     {
+                        Console.WriteLine("\t|__ pKIExtendedKeyUsage:");
                         for (int e = 0; e < EKUs.Count; e++)
                         {
-                            Console.WriteLine(new Oid(sr.Properties["pKIExtendedKeyUsage"][e].ToString()).FriendlyName);
+                            Console.WriteLine("\t|    |__ " + (new Oid(sr.Properties["pKIExtendedKeyUsage"][e].ToString()).FriendlyName));
                         }
                     }
                 }
