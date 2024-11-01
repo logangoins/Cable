4e7d36e633bc742d2f5f8098c5cc0bd4af731664
shellph1sh <LoganGoins@protonmail.com>
2024-11-01 14:22:05 -0500

    Added Asrep method to add and remove DONT_PRE_AUTH

--- Cable/Modules/Users.cs
+++ Cable/Modules/Users.cs
@@ -43,6 +43,56 @@ namespace Cable.Modules
             }
         }
 
+        public static void Asrep(string user, string type)
+        {
+            try
+            {
+                SearchResultCollection results;
+
+                DirectoryEntry de = new DirectoryEntry();
+                DirectorySearcher ds = new DirectorySearcher(de);
+
+                string query = "(samaccountname=" + user + ")";
+                ds.Filter = query;
+                results = ds.FindAll();
+
+                if (results.Count == 0)
+                {
+                    Console.WriteLine("[!] Cannot find account");
+                    return;
+                }
+
+                foreach (SearchResult sr in results)
+                {
+                    DirectoryEntry mde = sr.GetDirectoryEntry();
+                    int uac = (int)mde.Properties["useraccountcontrol"].Value;
+                    
+                    if (type == "add")
+                    {
+                        Boolean hasASREP = (Boolean)((uac & 0x400000) != 0);
+                        if (hasASREP)
+                        {
+                            Console.WriteLine("[!] " + user + " is already configured with DONT_REQ_PREAUTH");
+                            return;
+                        }
+                        mde.Properties["useraccountcontrol"].Value = uac | 0x400000;
+                        mde.CommitChanges();
+                        Console.WriteLine("[+] " + user + " configured with DONT_REQ_PREAUTH");
+                    }
+                    else if (type == "remove")
+                    {
+                        mde.Properties["useraccountcontrol"].Value = uac & ~0x400000;
+                        mde.CommitChanges();
+                        Console.WriteLine("[+] Removed DONT_REQ_PREAUTH on " + user);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[!] Failed to write userAccountControl attrbute: " + ex.Message);
+            }
+        }
+
         public static void removeSPN(string spn, string user)
         {
             try
